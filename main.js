2ce30bd2058b39488c7ffaa9072418dce13d0640
lucaGilardenghi <luca.gilardenghi@yahoo.co.uk>
2023-07-26 14:37:18 +0100

    Finished version for week2

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 document.getElementById("switch").addEventListener("click", () => {
-  const main = document.main;
-  if (main.classList.contains(light)) {
-    main.classList.replace("light", "dark");
+  let body = document.body;
+  let button = document.getElementById("switch");
+  if (body.classList.contains("light")) {
+    body.classList.replace("light", "dark");
+    button.innerHTML = "light";
   } else {
     body.classList.replace("dark", "light");
+    button.innerHTML = "dark";
   }
 });
